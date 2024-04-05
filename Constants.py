a928c848b3f2983460d987963f0b49bfb01196c1
Yaxel12 <163606758+Yaxel12@users.noreply.github.com>
2024-04-05 09:41:37 -0500

    Create Constants.py

---
+++ Constants.py
@@ -0,0 +1,15 @@
+
+HP = "HP"
+ATTACK = "Attack"
+DEFENSE = "Defense"
+SPATTACK = "SpAttack"
+SPDEFENSE = "SpDefense"
+SPEED = "Speed"
+
+
+
+DO_ATTACK = "attack"
+DO_ATTACK_SELECTION = "selected_attack"
+
+PHYSICAL = "physical"
+SPECIAL = "special"
